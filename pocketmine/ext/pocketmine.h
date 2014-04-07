27314c0e8465035dcc2b074eba48b71388737b10
Jenkins <shoghicp@pocketmine.net>
2014-04-07 12:12:08 -0400

    Updated extension from 4d4f50fb

--- pocketmine/ext/pocketmine.h
+++
@@ -1,9 +0,0 @@
-
-/* This file was generated automatically by Zephir do not modify it! */
-
-#ifndef ZEPHIR_CLASS_ENTRIES_H
-#define ZEPHIR_CLASS_ENTRIES_H
-
-#include "pocketmine/utils/random.h"
-
-#endif
