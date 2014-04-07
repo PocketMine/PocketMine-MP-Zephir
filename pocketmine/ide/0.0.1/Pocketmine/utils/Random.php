15d9949e2d760dc99fd7fee402e163b8ee56e870
root <root@jenkins-pocketmine.pocketmine.net>
2014-04-06 21:07:08 -0400

    Code generated \o/

---
+++ pocketmine/ide/0.0.1/Pocketmine/utils/Random.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PocketMine\Utils;
+
+class Random
+{
+	protected $z = 0;
+	protected $w = 0;
+
+	public function __construct()    {
+    }
+
+	public function setSeed()    {
+    }
+
+	/**
+	 * Returns an 31-bit integer (not signed)
+	 */
+	public function nextInt()    {
+    }
+
+	/**
+	 * Returns an 32-bit integer (signed)
+	 */
+	public function nextSignedInt()    {
+    }
+
+	public function nextFloat()    {
+    }
+
+	public function nextSignedFloat()    {
+    }
+
+	public function nextBoolean()    {
+    }
+
+	public function nextRange()    {
+    }
+
+}
