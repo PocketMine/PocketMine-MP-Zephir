5e1f65c5333280830517e2174b203ce17a8d617b
Jenkins <shoghicp@pocketmine.net>
2014-04-08 18:54:25 -0400

    Updated extension from ce8de1c7

---
+++ pocketmine/ext/kernel/concat.h
@@ -0,0 +1,31 @@
+#define ZEPHIR_CONCAT_SV(result, op1, op2) \
+	 zephir_concat_sv(&result, op1, sizeof(op1)-1, op2, 0 TSRMLS_CC);
+#define ZEPHIR_SCONCAT_SV(result, op1, op2) \
+	 zephir_concat_sv(&result, op1, sizeof(op1)-1, op2, 1 TSRMLS_CC);
+
+#define ZEPHIR_CONCAT_SVSVS(result, op1, op2, op3, op4, op5) \
+	 zephir_concat_svsvs(&result, op1, sizeof(op1)-1, op2, op3, sizeof(op3)-1, op4, op5, sizeof(op5)-1, 0 TSRMLS_CC);
+#define ZEPHIR_SCONCAT_SVSVS(result, op1, op2, op3, op4, op5) \
+	 zephir_concat_svsvs(&result, op1, sizeof(op1)-1, op2, op3, sizeof(op3)-1, op4, op5, sizeof(op5)-1, 1 TSRMLS_CC);
+
+#define ZEPHIR_CONCAT_SVSVSVS(result, op1, op2, op3, op4, op5, op6, op7) \
+	 zephir_concat_svsvsvs(&result, op1, sizeof(op1)-1, op2, op3, sizeof(op3)-1, op4, op5, sizeof(op5)-1, op6, op7, sizeof(op7)-1, 0 TSRMLS_CC);
+#define ZEPHIR_SCONCAT_SVSVSVS(result, op1, op2, op3, op4, op5, op6, op7) \
+	 zephir_concat_svsvsvs(&result, op1, sizeof(op1)-1, op2, op3, sizeof(op3)-1, op4, op5, sizeof(op5)-1, op6, op7, sizeof(op7)-1, 1 TSRMLS_CC);
+
+#define ZEPHIR_CONCAT_VS(result, op1, op2) \
+	 zephir_concat_vs(&result, op1, op2, sizeof(op2)-1, 0 TSRMLS_CC);
+#define ZEPHIR_SCONCAT_VS(result, op1, op2) \
+	 zephir_concat_vs(&result, op1, op2, sizeof(op2)-1, 1 TSRMLS_CC);
+
+#define ZEPHIR_CONCAT_VV(result, op1, op2) \
+	 zephir_concat_vv(&result, op1, op2, 0 TSRMLS_CC);
+#define ZEPHIR_SCONCAT_VV(result, op1, op2) \
+	 zephir_concat_vv(&result, op1, op2, 1 TSRMLS_CC);
+
+
+void zephir_concat_sv(zval **result, const char *op1, zend_uint op1_len, zval *op2, int self_var TSRMLS_DC);
+void zephir_concat_svsvs(zval **result, const char *op1, zend_uint op1_len, zval *op2, const char *op3, zend_uint op3_len, zval *op4, const char *op5, zend_uint op5_len, int self_var TSRMLS_DC);
+void zephir_concat_svsvsvs(zval **result, const char *op1, zend_uint op1_len, zval *op2, const char *op3, zend_uint op3_len, zval *op4, const char *op5, zend_uint op5_len, zval *op6, const char *op7, zend_uint op7_len, int self_var TSRMLS_DC);
+void zephir_concat_vs(zval **result, zval *op1, const char *op2, zend_uint op2_len, int self_var TSRMLS_DC);
+void zephir_concat_vv(zval **result, zval *op1, zval *op2, int self_var TSRMLS_DC);
